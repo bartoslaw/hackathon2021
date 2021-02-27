2a0e2d89da48dcf77bd627beb6bbc9250bce81d2
Norbert Hanysz <norberthanysz@gmail.com>
2021-02-27 15:49:07 +0100

    Add menu

---
+++ Assets/new_game_script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class new_game_script : MonoBehaviour
+{
+   
+   public void playGame() {
+       //TODO open new game
+   }
+
+}
